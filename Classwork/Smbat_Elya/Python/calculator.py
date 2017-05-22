017699d912503c861b02a2517cc1da85275e9353
Smbat <Smbat.Dallakyan@gmail.com>
2017-05-22 23:49:30 +0400

    Done : usage alternat "Swich", "argparse

--- Classwork/Smbat_Elya/Python/calculator.py
+++ Classwork/Smbat_Elya/Python/calculator.py
@@ -5,29 +5,30 @@ def sub(var1, var2):
 def mul(var1, var2):
     return var1 * var2
 def div(var1, var2):
+    if var2 == 0:
+        return "null"
     return var1 / var2
-
+    
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("-c", type=str)
+parser.add_argument("-c", type=str, help="Enter in parametrs < -c 5,+,8 >")
 args = parser.parse_args()
 list=args.c.split(',')
-print list[0]
 firstValue=int(list[0])
 action=list[1]
 secondValue=int(list[2])
 
-check=1
-if action == "+":
-    result = sum(firstValue, secondValue)
-if action == "-":
-    result = sub(firstValue, secondValue)
-if action == "*":
-    result = mul(firstValue, secondValue)
-if action == "/":
-    if 0 == secondValue:
-        print "INVALED VALUE, DIV BY ZERO\nStart agein"
-    result = div(firstValue, secondValue)
-if action != "+" and action != "-" and action != "*" and action != "/": 
-    print('INVALED ACTION\nStart agein')
-print firstValue, action, secondValue, "=", result
+actions = {
+    '+': sum(firstValue, secondValue),
+    '-': sub(firstValue, secondValue),
+    '*': mul(firstValue, secondValue),
+    '/': div(firstValue, secondValue)
+}
+
+
+if action != "+" and action != "-" and action != "*" and action != "/":
+    print('INVALED ACTION')
+elif action == '/' and secondValue == 0:
+    print('Div on 0')
+else:
+    print firstValue, action, secondValue, "=", actions[action]
