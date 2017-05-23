5bd33a0d4a22159aeb8bf508b7ddcc1ec9a6b8fa
Ghazaryan Khachik <x-ghazaryan@mail.ru>
2017-05-23 23:18:56 +0400

    Corrected json

--- Homework/Khachik/Python/jsonSecond.py
+++ Homework/Khachik/Python/jsonSecond.py
@@ -1,27 +1,54 @@
+#This program simulates the operation of an json
 
 arrkey=[]       #List of keys
 arrval=[]       #List of Values
+bracket=[]
 length = 0
-
-
-
+boolian = 1
 #This function checks the data
 def check(arg,key,length):
-    str1 = arg.split(':')
+    str1 = arg.split(':', 1)
     if len(str1) != 2:
         print "Eror:Wrong input"
-        exit(0)
+        return 0
     if str1[0][0] != '"' or str1[0][len(str1[0]) - 1] != '"':
-        print "Eror:Wrong input"
-        exit(0)
+        print "Eror:Invalid key"
+        return 0
     str1[0] = str1[0][1:]
     str1[0] = str1[0][:-1]
+    if str1[1][0] != '"' and str1[1][0] != "{" and str1[1][0] !="[":
+        for i in range(len(str1[1]) - 1):
+            if not isinstance( str1[1][i], int ):
+                print "Error:Wrong value(value is not valid)"
+                return 0
+    if str1[1][0] == '"':
+        if str1[1][len(str1[0]) - 1] != '"':
+            print "Error:Invalid value"
+            return 0
+        str1[1] = str1[1][1:]
+        str1[1] = str1[1][:-1]
+    if str1[1][0] == "{" or str1[1][0] == "[":
+        for i in range(len(str1[1])):
+            if str1[1][i] == "{" or str1[1][i] == "[":
+                bracket.append(str1[1][i])
+            if str1[1][i] == "}" or str1[1][i] == "]":
+                if str1[1][i] == "}" and bracket[len(bracket) - 1] != "{": 
+                    print "Error:Wrong brackets in value"
+                    return 0
+                if str1[1][i] == "]" and bracket[len(bracket) - 1] != "[": 
+                    print "Error:Wrong brackets in value"
+                    return 0
+                bracket.pop()
+        if len(bracket) != 0:
+            print "Error: Wrong barckets in value"
+            return 0
     for i in range(length):
         if str1[0] == arrkey[i]:
             print "Eror:Keys are repeated"
-            exit(0)
+            return 0
     arrkey.append(str1[0])
     arrval.append(str1[1])
+    return 1
 
 #This function findes value by key
 def find(key, length):
@@ -38,28 +65,12 @@ if string[0] != "{" or string[len(string) - 1] != "}":
     exit(0)
 string = string[1:]
 string = string[:-1]
-l = len(string)
-i = 0
-first = ""
-aposrt = 0
-while i < (l - 1):
-    i += 1
-#    print i
-    if string[i] == '"':
-        if aposrt == 1:
-            apostr = 0
-            key1 = string[:i]
-        else
-            apostr += 1
-    
-    
-
-'''
 list = string.split(',')
 l = len(list)
 for i in range(l):
-    check(list[i],key,length)
+    if not check(list[i],key,length):
+        boolian = 0
     length += 1
-find(key, length)
-print "Key not found"
-'''
+if boolian != 0:
+    find(key, length)
+    print "Key not found"
