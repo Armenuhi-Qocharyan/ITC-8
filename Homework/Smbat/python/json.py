828b30919a972e751578ae82bffc60997c2bb209
Smbat <Smbat.Dallakyan@gmail.com>
2017-05-23 23:59:21 +0400

    JSON validationi In Progress

--- Homework/Smbat/python/json.py
+++ Homework/Smbat/python/json.py
@@ -1,5 +1,37 @@
+def validation(json):
+    if json[0] != '{' and json[0] != '[':
+        return 0
+    scobes = ''
+    chakert = 0
+    verchaket = 0
+    for i in range(0, len(json)):
+        if chakert == 0:
+            if json[i] == '{' or  json[i] == '[':
+                scobes = scobes + json[i]
+            elif (json[i] == '}' and scobes[-1] == '{') or (json[i] == ']' and scobes[-1] == '['):
+                scobes = scobes[:-1]
+            elif (json[i] == '}' and scobes[-1] != '{') or (json[i] == ']' and scobes[-1] != '['):
+                return 0 
+            elif json[i] == '"':
+                chakert = 1
+        else:
+            print json[i]
+            if json[i] == '"':
+                chakert = 0
+                if verchaket == 0 and json[i+1] == ':':
+                    verchaket = 1
+                    i++
+
+            elif json[i].isalpha() == False and json[i].isdigit() == False:
+                print "In chakert Wrong Simbool"
+                return 0
+    return 1
+
 def main():
     inputJson = raw_input("Input Json >> ")
+    print validation(inputJson)
+    return
+
     if inputJson[0] != '{' or inputJson[-1] != '}':
         print "Wrong JSON! "
         return
