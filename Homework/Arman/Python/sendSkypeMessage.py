f56504ac5bbef32ce1559e962c5b395824a9d59a
Arman Avdalyan <avdalyan1999@gmail.com>
2017-05-24 00:37:39 +0400

    Added new version for skype message

--- Homework/Arman/Python/sendSkypeMessage.py
+++ Homework/Arman/Python/sendSkypeMessage.py
@@ -3,10 +3,9 @@ from random import randint
 import rstr, time
 
 while(1):
-    person=rstr.xeger(r'[A-Z][a-z]{3} [A-Z][a-z]{4}yan ')
-    person+=str(randint(1940, 2017))
+    person=rstr.xeger(r'[A-Z][a-z]{3} [A-Z][a-z]{4}yan 19[4-9][0-9]|200[0-9]|201[0-7]')
     print person
     client = Skype()
     client.Attach()
-    client.SendMessage("armenuhi_qocharyan_im", person)
+    client.SendMessage("skype", person)
     time.sleep(600)
