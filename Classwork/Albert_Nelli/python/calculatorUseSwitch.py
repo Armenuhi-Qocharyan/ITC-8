e83a6f165c50ad117e0739162b489265fd78bd26
Albert Aghajanyan <albert93aghajanyan@mail.ru>
2017-05-23 20:22:48 +0400

    Add calculatorUseSwitch.py file

---
+++ Classwork/Albert_Nelli/python/calculatorUseSwitch.py
@@ -0,0 +1,27 @@
+def add(first, second):
+    return first + second
+
+def sub(first, second):
+    return first - second
+
+def mul(first, second):
+    return first * second
+
+def div(first, second):
+    if second != 0 :
+        return first / second
+    else:
+        print "Division on 0 "
+        return
+
+first = input("Enter first number  : ")
+second = input("Enter second number: ")
+operator = raw_input("Enter choice(+ - * or /): ")
+
+switch = {
+    '+': add(first, second),
+    '-': sub(first, second),
+    '*': mul(first, second),
+    '/': div(first, second),
+}
+print "Result = ",(switch.get(operator, "Invaled Action"))
