f9123c39adb60443839b72eb734bfcdcdaa5a34c
Smbat <Smbat.Dallakyan@gmail.com>
2017-05-23 12:36:41 +0400

    Add method main() Json Without Validation

--- Homework/Smbat/python/json.py
+++ Homework/Smbat/python/json.py
@@ -1,9 +1,17 @@
-inputJson = raw_input("Input Json >> ")
-if inputJson[0] != '{' or inputJson[-1] != '}':
-    print "Wrong JSON! "
-inputJson = inputJson[1:-1]
-list = inputJson.split(',')
-json = {}
-for i in range(0,len(list)):
-    json[list[i].split(':')[0]] = list[i].split(':')[1] 
-print json['two']
+def main():
+    inputJson = raw_input("Input Json >> ")
+    if inputJson[0] != '{' or inputJson[-1] != '}':
+        print "Wrong JSON! "
+        return
+    inputJson = inputJson[1:-1]
+    list = inputJson.split(',')
+    json = {}
+    for i in range(0,len(list)):
+        key = list[i].split(':')[0]
+        value = list[i].split(':')[1]
+        json[key] = value 
+    
+    key = raw_input("Input KEY to get Value >> ")
+    print json[key]
+
+main()
