163d5cd786f54b7818fd9969f9136a7175eb71f6
Arman Avdalyan <avdalyan1999@gmail.com>
2017-05-24 00:53:42 +0400

    Added correct version of calculator

--- Homework/Arman/Python/calc.py
+++ Homework/Arman/Python/calc.py
@@ -1,26 +1,24 @@
 import argparse
+import re
 import sys
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-c")
 args = parser.parse_args()
 s=args.c
 
+def mysplit(mystr):
+    return re.split("([+-/*])", mystr.replace(" ", ""))
+
+arr=mysplit(s)
 opr=''
-for i in s:
-    if i=="+" or i=="-" or i=="*" or i=="/":
-        arr=s.split(i)
-        opr=i
-        break
 
-
-#can't use not operator
-if arr[0].isdigit() and arr[1].isdigit():
-    print ''
-else:
+if not(arr[0].isdigit() and arr[2].isdigit()):
     sys.exit("Please input only numbers")
 
 num1=int(arr[0])
-num2=int(arr[1])
+opr=arr[1]
+num2=int(arr[2])
 
 def sum (num1,num2):
     return num1+num2
@@ -36,21 +34,7 @@ def div (num1,num2):
         sys.exit("Second number can't be 0")
     else:
         return num1/num2
-        '''
-if opr=="+":
-    sum(num1, num2)
-else:
-    if opr=="-":
-        diff(num1, num2)
-    elif opr=="*":
-        mult(num1, num2)
-    elif opr=="/":
-        div(num1, num2)
-    else:
-        print "Wrong option"
 
-'''
-#use this but it's call all cases can't use break
 result = {
     '+': sum(num1, num2),
     '-': diff(num1, num2),
