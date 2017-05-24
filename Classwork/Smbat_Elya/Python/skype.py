d83d7996c35e2c5ec3c3e9e1179c9fc227fb6f00
Smbat <Smbat.Dallakyan@gmail.com>
2017-05-24 12:08:44 +0400

    Done task Skype

--- Classwork/Smbat_Elya/Python/skype.py
+++ Classwork/Smbat_Elya/Python/skype.py
@@ -7,7 +7,7 @@ client.Attach()
 user = 'armenuhi_qocharyan_im'
 
 while (True):
-    message = rstr.xeger('[A-Z][a-z]{3,8} [A-Z][a-z]{3,8}yan (19\d\d|20[0-1][0-7])\nHi Armenuhi, you are resive message from Smbat :)')
+    message = rstr.xeger('[A-Z][a-z]{3,8} [A-Z][a-z]{3,8}yan (19\d\d|20[0-1][0-7])\nHi Armenuhi you are reseve message from Smbat')
     client.SendMessage(user, message)
     print message
     time.sleep(600)
