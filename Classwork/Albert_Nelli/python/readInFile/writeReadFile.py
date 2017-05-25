ad3d0def91aa5f1a777564b414850c08ba2d55fa
Albert_Nelli <albert93aghajanyan@mail.ru>
2017-05-25 17:11:02 +0400

    Change writeReadFile.py file

--- Classwork/Albert_Nelli/python/readInFile/writeReadFile.py
+++ Classwork/Albert_Nelli/python/readInFile/writeReadFile.py
@@ -1,4 +1,4 @@
-def readInFile():
+def readFile():
     file = open("text.txt","r")
     myDict = {}
     for line in file:
@@ -12,12 +12,12 @@ def findValue(myDict):
     findKey = input("Enter key to find value: ")
     for key,val in myDict.items():
         if findKey == key:
-            print key,val
+            print key," => ",val
             return val
     else:
             print "Key not found"
             return None
 
 if __name__ == '__main__':
-    readInFile()
+    readFile()
 
