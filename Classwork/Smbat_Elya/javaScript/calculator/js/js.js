32ac6611b985bc350d6e38c247e8b225bdc8354c
Smbat <Smbat.Dallakyan@gmail.com>
2017-06-19 18:06:55 +0400

    js calculatre

---
+++ Classwork/Smbat_Elya/javaScript/calculator/js/js.js
@@ -0,0 +1,8 @@
+/**
+ * Created by student on 6/19/17.
+ */
+var screen = document.getElementById("screen");
+
+var  inputNumber = function(btnThis) {
+    screen.value = screen.value + btnThis.innerHTML;
+}
