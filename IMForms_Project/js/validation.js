287c1f264946aeeb1c52960edac8beeea70c0206
Liana Andreasyan <liana.andreasyan.94@gmail.com>
2017-06-22 01:19:41 +0400

    updated validation in registration page

--- IMForms_Project/js/validation.js
+++ IMForms_Project/js/validation.js
@@ -1,46 +1,64 @@
+var checkEmail = true;
+var checkPsw = true;
+var checkUname = true;
+var checkRepeat = true;
+
+
 function validator () {
-
-    var username = document.getElementById("user").value;
-    var password = document.getElementById("password").value;
-    var repeat = document.getElementById("repeat").value;
-    var email = document.getElementById("email").value;
-    var checkEmail = validateEmail(email);
-    var checkPassword = validatePassword(password);
-
-    if (checkEmail == false) {
-        document.getElementById("alertEmail").style.display="block";
-    } else {
-        document.getElementById("alertEmail").style.display="none";
-    }
-
-    if (checkPassword == false) {
-        document.getElementById("alertPsw").style.display="block";
-    } else {
-        document.getElementById("alertPsw").style.display="none";
-    }
-
-    if (repeat != password) {
-        document.getElementById("alertRepeat").style.display="block";
-    } else {
-        document.getElementById("alertRepeat").style.display="none";
-    }
-
-    if (username.length > 15) {
-        document.getElementById("alertUname").style.display="block";
-    } else {
-        document.getElementById("alertUname").style.display="none";
-    }
-
+    if (true == checkEmail && true == checkPsw && true == checkUname && true == checkRepeat) {
+        document.getElementById("submit").disabled=false;
+    }   
 }
 
 
 function validateEmail(email) {
     var regularExpression = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return regularExpression.test(email);
+    if (!regularExpression.test(email)) {
+        document.getElementById("alertEmail").style.display="block";
+        document.getElementById("submit").disabled=true;
+        checkEmail=false;
+    } else {
+        document.getElementById("alertEmail").style.display="none";
+        checkEmail=true;
+        validator();
+    }
 }
 
 function validatePassword(password) {
     var regularExpression  = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
 
-    return regularExpression.test(password);
+    if(!regularExpression.test(password)) {
+        document.getElementById("alertPsw").style.display="block";
+        document.getElementById("submit").disabled=true;
+        checkPsw=false;
+    } else {
+        document.getElementById("alertPsw").style.display="none";
+        checkPsw=true;
+        validator();
+    }
+}
+
+function validateRepeat (repeat) {
+    var password = document.getElementById("password").value;
+    if (repeat != password) {
+        document.getElementById("alertRepeat").style.display="block";
+        document.getElementById("submit").disabled=true;
+        checkRepeat=false;
+    } else {
+        document.getElementById("alertRepeat").style.display="none";
+        checkRepeat=true;
+        validator();
+    }
+}
+
+function validateUsername (username) {
+    if (username.length > 15) {
+        document.getElementById("alertUname").style.display="block";
+        document.getElementById("submit").disabled=true;
+        checkUname=false;
+    } else {
+        document.getElementById("alertUname").style.display="none";
+        checkUname=true;
+        validator();
+    }
 }
