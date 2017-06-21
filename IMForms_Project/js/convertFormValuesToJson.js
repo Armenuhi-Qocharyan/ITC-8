055bdd5d8070432622173805575eb00f010c8d56
SmbatDallakyan <Smbat.Dallakyan.im@gmail.com>
2017-06-21 23:28:43 +0400

    correct script convertFromValueToJson

--- IMForms_Project/js/convertFormValuesToJson.js
+++ IMForms_Project/js/convertFormValuesToJson.js
@@ -2,11 +2,10 @@ var getFormValues = function () {
     var inputs = document.getElementsByClassName("inputs");
     var jsontext = "{";
     for (var i = 0, length = inputs.length; length > i; i++) {
-        jsontext += inputs[i].name + ":" + inputs[i].value + ",";
+        jsontext += '"' + inputs[i].name + '"' + ":" + '"' + inputs[i].value + '"' + ",";
     }
     jsontext += "}";
-    console.log(jsontext);
-    json = JSON.parse(jsontext);
+    alert(jsontext);
 }
 
 var submitBtn = document.getElementById("submit_btn");
