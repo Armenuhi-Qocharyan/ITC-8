9929a38176ba7a9ad4ea6a7e5f1c11d60f5800d8
Liana Andreasyan <liana.andreasyan.94@gmail.com>
2017-06-25 16:02:21 +0400

    added swap functionality for form elements, hide and show effects for remove button

--- IMForms_Project/js/create.js
+++ IMForms_Project/js/create.js
@@ -1,35 +1,101 @@
-global="";
+global = "";
 cnt = 0;
+
+function newDrop(event) {
+    event.preventDefault();
+    var drop_target = event.target;
+    var drag_target_id = event.dataTransfer.getData('target_id');
+    var testId = /myid/;
+    if (false == testId.test(drop_target.id)) {
+        drop_target = drop_target.parentNode;
+    }
+    var drag_target = $('#' + drag_target_id)[0];
+    if (drag_target.classList.contains('hideDiv')) {
+        var tmp = document.createElement('span');
+        tmp.className = 'hide';
+        drop_target.before(tmp);
+        drag_target.before(drop_target);
+        tmp.replaceWith(drag_target);
+    }
+}
+
+function newDrag(event) {
+    event.dataTransfer.setData('target_id', event.target.id);
+}
+
+
+function createDragable(newDiv) {
+    newDiv.ondragstart = function(event) {
+        newDrag(event);
+    };
+    newDiv.ondragover = function(event) {
+        allowDrop(event);
+    };
+    newDiv.ondrop = function(event) {
+        newDrop(event);
+    };
+}
 
 function Add(myId) {
     myId = "Element" + myId;
     var myDiv = document.getElementById(myId);
-    var newDiv = document.createElement( 'div' );
+    var newDiv = document.createElement('form');
     newDiv.innerHTML = myDiv.innerHTML;
- //   newDiv.className="hideDiv";
-    newDiv.setAttribute("id","myid"+ cnt);
-    newDiv.setAttribute("class","hideDiv");
+    newDiv.setAttribute("id", "myid" + cnt);
+    newDiv.setAttribute("class", "hideDiv");
     newDiv.name = myId;
     document.getElementById("dropArea").appendChild(newDiv);
     $("#drag").hide();
     cnt++;
+    newDiv.onmouseover = function () {
+    	mouseOnbutton(newDiv);
+    }
+    newDiv.onmouseleave = function () {
+
+	mouseOutButton(newDiv);
+    }
+    newDiv.draggable = true;
+    createDragable(newDiv);
+
+
+
 }
 
+function mouseOnbutton (newDiv) {
+   var allChildNodes = newDiv.getElementsByTagName('button');
+   var len = allChildNodes.length - 1;
+   allChildNodes[len].style.display="initial";
+}
+
+
+function mouseOutButton (newDiv) {
+   var allChildNodes = newDiv.getElementsByTagName('button');
+   var len = allChildNodes.length - 1;
+   allChildNodes[len].style.display="none";
+}
+
+
 function Remove(This) {
-    $( "#"+ This.parentNode.id ).remove();
+    cnt--;
+    $("#" + This.parentNode.id).remove();
+    if (cnt == 0) {
+        $("#drag").show();
+    }
 }
 
 function allowDrop(ev) {
     ev.preventDefault();
 }
 
-function drag(ev,otherId) {
+function drag(ev, otherId) {
     ev.dataTransfer.setData("text", ev.target.id);
     global = otherId;
 
 }
 
 function drop(ev) {
+    if (global == "") {
+        return;
+    }
     Add(global);
 }
-
--- IMForms_Project/js/edu.js
+++ IMForms_Project/js/edu.js
@@ -1,33 +1,32 @@
 var counter = 1;
 var limit = 3;
-
-function addInput(divName){
+/*
+function addInput(childId){
     if (counter == limit)  {
         alert("You have reached the limit of adding " + counter + " inputs");
     }
     else {
-        var div = document.getElementById("new"), 
-            clone = div.cloneNode(true);
-        document.getElementById(divName).appendChild(clone);
+        var child = document.getElementById(childId), 
+            clone = child.cloneNode(false);
+		var Parent = child.parentNode.id; 
+        document.getElementById(Parent).appendChild(clone);
         counter++;
     }
 }
-
-function enable() {
-    document.getElementById("plus").disabled=false;
-    var allChildNodes = document.getElementById("list").getElementsByTagName('*');
-    for(var i = 0; i < allChildNodes.length; i++) {
+*/
+function enable(Id) {
+    document.getElementById("plus").disabled = false;
+    var allChildNodes = document.getElementById(Id).getElementsByTagName('*');
+    for (var i = 0; i < allChildNodes.length; i++) {
         allChildNodes[i].disabled = false;
     }
 }
 
-function disable() {
- //    document.getElementById("list").style.display="none";
-    document.getElementById("plus").disabled=true;
-    var allChildNodes = document.getElementById("list").getElementsByTagName('*');
-    for(var i = 0; i < allChildNodes.length; i++) {
-        allChildNodes[i].disabled = true;
+function disable(Id) {
+    var allChildNodes = document.getElementById(Id).getElementsByTagName('*');
+    for (var i = 0; i < allChildNodes.length; i++) {
+        if (allChildNodes[i].id != "radiobtn1" && allChildNodes[i].id != "remove") {
+            allChildNodes[i].disabled = true;
+        }
     }
-
 }
-
