80520e42ea9828e8802979d1bcb076559b019b29
Albert Aghajanyan <albert93aghajanyan@mail.ru>
2017-06-26 23:38:37 +0400

    Have corrected the game BattleShip

--- Homework/Albert/JavaScript/BattleShip/javascript/battleShip.js
+++ Homework/Albert/JavaScript/BattleShip/javascript/battleShip.js
@@ -1,35 +1,36 @@
-var Ships = function(size, count) {
+var Ships = function(size, count, id) {
     this.size = size;
     this.count = count;
+    this.id = id;
 };
 Ships.prototype.print = function(obj) {
-    var ship = "<img src='images/ship3.png' class='img' id= " + obj.size + " draggable='true' ondragstart='drag(event)' style='width:" + obj.size * 40 + "px '>";
+    var ship = "<img src='images/ship3.png' class='img' id= "+this.id+" draggable='true' ondragstart='drag(event)' style='width:" + obj.size * 40 + "px '>";
     document.getElementById("forms").innerHTML += ship;
 };
 Ships.prototype.checkCount = function(obj) {
     if(obj.count === 0) {
-        //document.getElementById(obj.size).style.display = none;
         var removeShipImg = document.getElementById(obj.size);
         removeShipImg.parentNode.removeChild(removeShipImg);
     }
 };
-var ship1 = new Ships(1, 4);
-var ship2 = new Ships(2, 3);
-var ship3 = new Ships(3, 2);
-var ship4 = new Ships(4, 1);
+var ship1 = new Ships(1, 4, '1');
+var ship2 = new Ships(2, 3, '2');
+var ship3 = new Ships(3, 2, '3');
+var ship4 = new Ships(4, 1, '4');
 
 function createForm(row, column, id) {
-    var table = "<table id="+ id + '1'+" class='myTable' cellpadding='22' cellspacing='2'>";
+    var table = "<table id='gamer_11' class='myTable' cellpadding='22' cellspacing='2'>";
     for (var i = 1; i <= row; ++i) {
         table += "<tr>";
         for (var j = 1; j <= column; ++j) {
-            table += "<td class='tdClass' ondrop='drop(event)' ondragover='allowDrop(event)' onclick='find(this)'></td>";
+            table += "<td class='tdClass'  onclick='findShips(this)' ondrop='drop(event)' ondragover='allowDrop(event)'></td>";
         }
         table += "</tr>";
     }
     table += "</table>";
     document.getElementById(id).innerHTML += table;
 }
+
 function startGame() {
     document.getElementById("shipImage").style.display = "none";
     document.getElementById("start").style.display = "none";
@@ -45,6 +46,14 @@ function startGame() {
 var start = 0;
 var size = 0;
 var id = "";
+var arr = [];
+
+for(var i = 0; i < 10; i++){
+    arr[i] = [];
+    for(var j = 0; j < 10; j++){
+        arr[i][j] = 0;
+    }
+}
 
 function allowDrop(ev) {
     ev.preventDefault();
@@ -84,13 +93,6 @@ function checkLocation(size, id) {
     id = "" + x + (y + 1);
     return checkLocation(size -1, id)
 }
-var arr = [];
-for(var i = 0; i < 10; i++){
-    arr[i] = [];
-    for(var j = 0; j < 10; j++){
-        arr[i][j] = 0;
-    }
-}
 
 function printShips(size, id) {
     if (checkLocation(size, id)) {
@@ -106,6 +108,7 @@ function printShips(size, id) {
             document.getElementById(printId).style.backgroundImage = "url('images/ship7.png')";
             document.getElementById(printId).style.backgroundSize = "cover";
             document.getElementById(printId).style.backgroundColor = "#1efcff";
+            //document.getElementById(printId).setAttribute('class', 'printShips');
         }
         count(size);
         start+=1;
@@ -157,20 +160,24 @@ function Play() {
             document.getElementById(i + '' + j).style.backgroundImage = "url(images/harc.png";
             document.getElementById(i + '' + j).style.backgroundSize = "cover";
             document.getElementById(i + '' + j).style.backgroundColor = "#1efcff";
+            //document.getElementById(i + '' + j).setAttribute('class', 'play');
         }
     }
+    document.getElementById('start1').style.visibility = 'hidden';
 }
 
 var win = 0;
-function find(td) {
+
+function findShips(td) {
     var idTD = td.id;
     var x = Math.floor(idTD/10);
     var y = idTD%10;
-    if(document.getElementById('start1').style.visibility === 'visible'){
+    if(document.getElementById('start1').style.visibility === 'hidden'){
         if(arr[x][y] ===  1){
             document.getElementById(x +''+ y).style.backgroundImage = "url('images/x.jpeg')";
+            arr[x][y] = 2;
             win++;
-        } else {
+        } else if(arr[x][y] ===  0){
             document.getElementById(x + '' + y).style.backgroundImage = "url('images/o1.png')";
             document.getElementById(x + '' + y).style.backgroundColor = "#ffffff";
         }
@@ -180,7 +187,9 @@ function find(td) {
         return 0;
     }
 }
+
 function newGame() {
+    document.getElementById('start1').style.visibility = 'visible';
     document.getElementById('start1').innerHTML = "New Game";
     document.getElementById("forms").style.display = "none";
     document.getElementById("gamer_1").style.display = "none";
