f618563070a6553184ec1e3179793301ddab4ad5
Liana Andreasyan <liana.andreasyan.94@gmail.com>
2017-06-27 11:00:24 +0400

    some corrections

--- Homework/Liana/web/js/game/js/script.js
+++ Homework/Liana/web/js/game/js/script.js
@@ -2,34 +2,34 @@ var rotation,
     height,
     counter = 4,
     clickCount = 0,
-	shout = 0,
-	point = 0,
-	
-     ships = [{
-        "name": "carrier",
-        "length": 4,
-        "possition": 1,
-        "count": 1
-    },
-    {
-        "name": "battleship",
-        "length": 3,
-        "possition": 1,
-        "count": 2
-    },
-    {
-        "name": "cruiser",
-        "length": 2,
-        "possition": 1,
-        "count": 3
-    },
-    {
-        "name": "frigate",
-        "length": 1,
-        "possition": 1,
-        "count": 4
-    }
-];
+    shout = 0,
+    point = 0,
+
+    ships = [{
+            "name": "carrier",
+            "length": 4,
+            "possition": 1,
+            "count": 1
+        },
+        {
+            "name": "battleship",
+            "length": 3,
+            "possition": 1,
+            "count": 2
+        },
+        {
+            "name": "cruiser",
+            "length": 2,
+            "possition": 1,
+            "count": 3
+        },
+        {
+            "name": "frigate",
+            "length": 1,
+            "possition": 1,
+            "count": 4
+        }
+    ];
 
 function deleteShip() {
     var index;
@@ -37,16 +37,16 @@ function deleteShip() {
         if (height === ships[i].length) {
             index = ships[i].name;
             ships[i].count -= 1;
-			document.getElementById(index).innerHTML = ships[i].name + " (" + ships[i].count + ")";
+            document.getElementById(index).innerHTML = ships[i].name + " (" + ships[i].count + ")";
             if (ships[i].count === 0) {
                 document.getElementById(index).remove();
                 counter--;
             }
         }
     }
-	
+
     if (0 === counter) {
-		tableCreate("table2");
+        tableCreate("table2");
         eventListener('table2');
     }
 }
@@ -73,57 +73,57 @@ function second(cell) {
     var col = cell.cellIndex,
         row = cell.parentNode.rowIndex,
         table1Cell = document.getElementById('table1').rows[row].cells[col];
-        table2Cell = document.getElementById('table2').rows[row].cells[col];		
+    table2Cell = document.getElementById('table2').rows[row].cells[col];
     if (table1Cell.value === 2) {
         table1Cell.className = "right";
-		table2Cell.className = "yes";
-		table2Cell.onclick = "";
-		point++;
+        table2Cell.className = "yes";
+        table2Cell.onclick = "";
+        point++;
     } else {
         table1Cell.className = "wrong";
-		table2Cell.className = "no";
-	    table2Cell.onclick = "";
-		shout++;
+        table2Cell.className = "no";
+        table2Cell.onclick = "";
+        shout++;
     }
-	
-	if (point == 20) {
-		document.getElementById("table1").style.display = "none";
-	    document.getElementById("table2").style.display = "none";
-		document.body.className = "win";
-	} else if (shout == 20) {
-		document.getElementById("table2").style.display = "none";
-		document.getElementById("table1").style.display = "none";
-		document.body.className = "lose";
-	}
+
+    if (point == 20) {
+        document.getElementById("table1").style.display = "none";
+        document.getElementById("table2").style.display = "none";
+        document.body.className = "win";
+    } else if (shout == 20) {
+        document.getElementById("table2").style.display = "none";
+        document.getElementById("table1").style.display = "none";
+        document.body.className = "lose";
+    }
 }
 
-function isFree (cell) {
-    var row = cell.parentNode.rowIndex, 
-        table1 = document.getElementById("table1"), 
+function isFree(cell) {
+    var row = cell.parentNode.rowIndex,
+        table1 = document.getElementById("table1"),
         col = cell.cellIndex,
         maxHeight,
         maxLength,
         minCol = (col > 0) ? (col - 1) : (col),
         minRow = (row > 0) ? (row - 1) : (row);
-    if ( rotation == 1 ) {
+    if (rotation == 1) {
         maxHeight = (row < 9) ? (row - minRow + 2) : (2);
         maxLength = (col + height < 10) ? (height + col - minCol + 1) : (height + 1);
     } else {
         maxHeight = (row + height < 10) ? (height + row - minRow + 1) : (height + 1);
         maxLength = (col < 9) ? (col - minCol + 2) : (2);
     }
-	
-	if (maxHeight + row - 1 > 10 || maxLength + col - 1 > 10 ) {
-		alert ("invalid possition");
-		rotation = 0;
-		return;
-	}
-	
+
+    if (maxHeight + row - 1 > 10 || maxLength + col - 1 > 10) {
+        alert("invalid possition");
+        rotation = 0;
+        return;
+    }
+
     for (var i = 0; i < maxHeight; ++i) {
         for (var j = 0; j < maxLength; ++j) {
             if (table1.rows[minRow + i].cells[minCol + j].value == 2) {
-				alert ("Invalid possition");
-				rotation = 0;
+                alert("Invalid possition");
+                rotation = 0;
                 return;
             }
         }
@@ -151,7 +151,7 @@ function eventListener(id) {
 
 function tableCreate(id) {
     var Parent = document.getElementById("tables"),
-    table = document.createElement('table');
+        table = document.createElement('table');
     table.setAttribute("id", id);
 
     for (var i = 0; i < 10; i++) {
