00d2f174c901540e628d621c1a6a06070d3b4905
Vrezh Unanyan <vrezhunanyan1997@mail.ru>
2017-06-28 00:18:38 +0400

    Add new JavaScriot extension file.

---
+++ Homework/Vrezh/js/extension/content.js
@@ -0,0 +1,4 @@
+var images = document.getElementsByTagName('img');
+for (var i = 0, l = images.length; i < l; i++) {
+    images[i].src = "http://placekitten.com.s3.amazonaws.com/homepage-samples/200/140.jpg";
+}
