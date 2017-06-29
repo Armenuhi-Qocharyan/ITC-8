ec51109b2b4c117ce7bba7451454beca9bd7e22d
Nelli <nelli.melkonyan.97@mail.ru>
2017-06-29 23:17:45 +0300

    Finished extension test

--- Homework/Nelli/JS/extension/changeImages.js
+++ Homework/Nelli/JS/extension/changeImages.js
@@ -3,6 +3,6 @@ function changeImage() {
     var allImages = document.getElementsByTagName("img");
     var imgLengt = allImages.length;
     for (var i = 0; i < imgLengt; ++i) {
-        allImages[i].src = "https://www.bmw.ua/content/dam/bmw/common/home/teaser/BMW-5series-sedan-home-teaser-s2-XXL-desktop.jpg";
+        allImages[i].src = "http://freedesignfile.com/upload/2015/09/Cute-smile-emoticon-icons-vectors-set-07.jpg";
     }
 }
