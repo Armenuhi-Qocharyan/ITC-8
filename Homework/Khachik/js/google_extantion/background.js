a4269cecc285e54a8a8586c54aa2f6b9bfb83183
Khachik Ghazaryan <Khachik.ghazaryan.im@gmail.com>
2017-06-30 12:42:42 +0400

    Corrected google extansion

--- Homework/Khachik/js/google_extantion/background.js
+++ Homework/Khachik/js/google_extantion/background.js
@@ -2,44 +2,15 @@ chrome.runtime.onMessage.addListener(
         function(request, sender, sendResponse) {
             switch (request.select) {
                 case "change": 
-                    chrome.tabs.query({
-                        active: true,
-                    currentWindow: true
-                    }, function (tabs) {
-                        chrome.tabs.sendMessage(
-                            tabs[0].id,
-                            {subject: 'change'},
-
-                            );
+                    chrome.tabs.query({active: true,currentWindow: true}, function (tabs) {
+                        chrome.tabs.sendMessage(tabs[0].id,{subject: 'change'},);
                     });
                     break;
                 case "cancel":
-                    chrome.tabs.query({
-                        active: true,
-                        currentWindow: true
-                    }, function (tabs) {
-                        chrome.tabs.sendMessage(
-                            tabs[0].id,
-                            {subject: 'cancel'},
-
-                            );
+                    chrome.tabs.query({active: true,currentWindow: true}, function (tabs) {
+                        chrome.tabs.sendMessage(tabs[0].id,{subject: 'cancel'},);
                     });
                     break;
-                case "original":
-                    chrome.tabs.query({
-                        active: true,
-                        currentWindow: true
-                    }, function (tabs) {
-                        chrome.tabs.sendMessage(
-                            tabs[0].id,
-                            {subject: 'original'},
-
-                            );
-                    });
-                    break;
-
-
-
             }
 
 });
