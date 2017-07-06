f6af07de0e85ef016b56d71b94161616cf1ddff6
rubenghalayan <rubenghalayan@gmail.com>
2017-07-06 04:03:06 +0400

    Added correct cv.html

--- IMForms_Project_AngularJS/app/controllers/createCtrl.js
+++ IMForms_Project_AngularJS/app/controllers/createCtrl.js
@@ -1,3 +1,15 @@
 createApp.controller("sideNav", function($scope) {
     $scope.items = ["FullName", "Email", "Address", "Phone", "Date of Birth", "Education", "Skills", "Work Experience", "Submit", "Dropdown", "Gender", "Hobby", "Image"];
 });
+
+
+createApp.controller("sideBar", function($scope, draggableData) {
+    $scope.Data = [];
+    for(var i = 0,len = draggableData.length; i < len; ++i){
+        $scope.data = {};
+        $scope.data.idName = draggableData[i].id;
+        $scope.data.className = draggableData[i].class;
+        $scope.data.name = draggableData[i].name;
+        $scope.Data.push($scope.data);
+    }
+});
