cbc6dcd021cddcef7722b551c9ca5db6b4209c17
Ruben <rubenghalayan@mail.ru>
2017-07-17 00:59:03 +0400

    Added mongodb homework with python

--- Homework/Ruben/sql/mongo/users.py
+++ Homework/Ruben/sql/mongo/users.py
@@ -1,16 +1,24 @@
+import argparse
 import pymongo
+import pprint
 from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--host', nargs='*',  default='[[localhost]]', help='Enter host')
+parser.add_argument('--port', nargs='*', type=int,  default='[[27017]]', help='Enter port')
+args = parser.parse_args()
+host = args.host
+port = args.port
+
+client = MongoClient(host[0], port[0])
 client.User.Student.drop()
 client.User.Student.insert({"Name":"Ruben", "ITC":8, "Exams":{"a":{"Subject":"C++", "Score":6}, "b":{"Subject": "Python", "Score":8}}})
 client.User.Student.insert({"Name":"Vachagan", "ITC":8, "Exams":{"a":{"Subject":"C++", "Score":8}, "b":{"Subject": "Python", "Score":10}}})
-client.User.Student.insert({"Name":"Smbat", "ITC":7, "Exams":{"a":{"Subject":"C++", "Score":8}, "b": {"Subject": "Python", "Score":9}}})
-import pprint
-pipe1 = [{"$group":{"_id":"$Name", "AVG":{"$avg": "$Exams.a.Score"}}}]
+client.User.Student.insert({"Name":"Smbat", "ITC":7, "Exams":{"a":{"Subject":"C++", "Score":8}, "b":{"Subject": "Python", "Score":9}}})
+pipe1 = [{"$group":{"_id" : {"Name":'$Name',"ITC":'$ITC'},"AVG":{"$avg":'$Exams.a.Score'}}}]
+pipe2 = [{"$group":{"_id":{"ITC":"$ITC", "Subject":"$Exams.a.Subject"}, "MIN":{"$min": "$Exams.a.Score"}, "MAX":{"$max":"$Exams.a.Score"}, "Count":{"$sum":1}}}]
+
 print("Avg Scores")
 pprint.pprint(list(client.User.Student.aggregate(pipe1)))
-pipe2 = [{"$group":{"_id":"$Exams.a.Subject", "MIN":{"$min": "$Exams.a.Score"}}}]
-pipe3 = [{"$group":{"_id":"$Exams.b.Subject", "MIN":{"$min": "$Exams.b.Score"}}}]
-print("MIN Scores")
+print("MIN and MAX Scores")
 pprint.pprint(list(client.User.Student.aggregate(pipe2)))
-pprint.pprint(list(client.User.Student.aggregate(pipe3)))
