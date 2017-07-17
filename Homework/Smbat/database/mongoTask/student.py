c7080e0d5921482d7b0cca77748cd927ea8ea3e2
SmbatDallakyan <Smbat.Dallakyan.im@gmail.com>
2017-07-18 01:16:39 +0400

    Mongodb update task python

--- Homework/Smbat/database/mongoTask/student.py
+++ Homework/Smbat/database/mongoTask/student.py
@@ -1,29 +1,43 @@
-import pymongo
 import argparse
-import pprint
 from pymongo import MongoClient
 
 def getDB():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--host", default = "localhost", help = "host", type = str) 
-    parser.add_argument("--port", default = "27017", help = "port", type = int);
+    parser.add_argument("--host", default = "localhost", help = "type --host <host>", type = str) 
+    parser.add_argument("--port", default = 27017, help = "type --port <port>", type = int);
+
     args = parser.parse_args()
-    client = MongoClient('localhost', 27017)    
+    host = args.host
+    port = args.port
+
+    client = MongoClient(host, port)
     client.studentsDb.studentsColl.drop()
     db = client.studentsDb
     return db
 
 def addCollection(db):
-    db.studentsColl.insert({"name" : "aaa","test":"1","surname":"aaaaa","exams":[{"itc":1,"subject":"c++",   "score":7},{"itc":2,"subject":"c++","score":9}]})
-    db.studentsColl.insert({"name" : "bbb","test":"3","surname":"bbbbb","exams":[{"itc":3,"subject":"python","score":5}, {"itc":4,"subject":"python","score":8}]})
-    db.studentsColl.insert({"name" : "ccc","test":"3","surname":"ccccc","exams":[{"itc":5,"subject":"JS","score":6}, {"itc":5,"subject":"c++","score":7}, {"itc":6,"subject":"JS","score":8}]})
+    db.studentsColl.insert({"name" : "aaa","surname":"aaaaa","exams":[{"itc":1,"subject":"c++",   "score":7},{"itc":2,"subject":"c++","score":9}]})
+    db.studentsColl.insert({"name" : "bbb","surname":"bbbbb","exams":[{"itc":3,"subject":"python","score":5}, {"itc":4,"subject":"python","score":8}]})
+    db.studentsColl.insert({"name" : "ccc","surname":"ccccc","exams":[{"itc":5,"subject":"JS","score":6}, {"itc":5,"subject":"c++","score":7}, {"itc":6,"subject":"JS","score":8}]})
+    db.studentsColl.insert({"name" : "ddd","surname":"ddddd","exams":[{"itc":1,"subject":"JS","score":4}, {"itc":5,"subject":"c++","score":7}, {"itc":6,"subject":"python","score":8}]})
 
 def nameItcScore(db):
-    for i in db.studentsColl.aggregate([{"$unwind":"$exams"},{"$group": {"_id":{"name":"$name","itc":"$exams.itc"}, "avg":{"$avg": "$exams.score"}}}]):
-        print i
+    for result in db.studentsColl.aggregate([{"$unwind":"$exams"},{"$group": {"_id":{"name":"$name","itc":"$exams.itc"}, "avg":{"$avg": "$exams.score"}}}]):
+        print result
+
+def studentSubject(db):
+    for result in db.studentsColl.aggregate([{"$unwind":"$exams"},{"$group": {"_id":{"name":"$name","surname":"$surname","subject":"$exams.subject"}, "maxScore":{"$max":"$exams.score"}, "minScore":{"$min":"$exams.score"},"countItc":{"$sum":1}}}]):
+        print result
+
+def itcSubjectMax(db):
+    for result in db.studentsColl.aggregate([{"$unwind":"$exams"},{"$group": {"_id":{"itc":"$exams.itc", "subject":"$exams.subject"}, "max":{"$max": "$exams.score"}, "studentCount":{"$sum":1}}}]):
+        print result
 
 if __name__ == "__main__":
-        db = getDB() 
-        addCollection(db)
-        nameItcScore(db)
-
+    db = getDB() 
+    addCollection(db)
+    nameItcScore(db)
+    print ""
+    studentSubject(db)
+    print ""
+    itcSubjectMax(db)
