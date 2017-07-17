d5640b3e0415b8532a52c18479b45e6faf48e46d
Khachik Ghazaryan <Khachik.ghazaryan.im@gmail.com>
2017-07-17 22:30:05 +0400

    corrected homework for 13/07

--- Homework/Khachik/db/13-07/query.sql
+++ Homework/Khachik/db/13-07/query.sql
@@ -1,10 +1,10 @@
 /*Task 1: Create query which got data from database in following format [name][surname][itc][average score]*/
 
-select name, surname, ITC, AVG(score) as average_score  from exams inner join students on student_id=students.id inner join itc on itc_id=itc.id group by name, surname, itc;
+select name, surname, ITC, AVG(score) as average_score  from exams, students  inner join itc on itc_id=itc.id where student_id=students.id group by name, surname, itc;
 
 /*Task 2: Create query which got data from database in following format [name][surname][itc][min score][max score][average score][itc number]*/
 
-select name, surname, ITC, MIN(score), MAX(score), AVG(score),COUNT(DISTINCT itc)  from exams inner join students on student_id=students.id inner join itc on itc_id=itc.id group by name, surname, ITC;
+select name, surname, ITC, MIN(score), MAX(score), AVG(score),COUNT(DISTINCT itc)  from exams, students  inner join itc on itc_id=itc.id where student_id=students.id group by name, surname, ITC;
 
 /*Task 1: Create query which got data from database in following format [itc][subject][max score][min score][exams number]*/
 
