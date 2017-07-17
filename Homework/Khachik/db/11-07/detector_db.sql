4859f7e8a98dce4d6053c4bfd747eb87447e7d73
Khachik Ghazaryan <Khachik.ghazaryan.im@gmail.com>
2017-07-17 21:24:18 +0400

    Added homework for 11/07

---
+++ Homework/Khachik/db/11-07/detector_db.sql
@@ -0,0 +1,134 @@
+-- MySQL dump 10.13  Distrib 5.5.55, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: detector_db
+-- ------------------------------------------------------
+-- Server version	5.5.55-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cameras`
+--
+
+DROP TABLE IF EXISTS `cameras`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cameras` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `camera_name` varchar(50) NOT NULL,
+  `longitude` float NOT NULL,
+  `latitude` float NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  `description` varchar(124) DEFAULT NULL,
+  `last_run_date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cameras`
+--
+
+LOCK TABLES `cameras` WRITE;
+/*!40000 ALTER TABLE `cameras` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cameras` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `images`
+--
+
+DROP TABLE IF EXISTS `images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `images` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `image_album_path` varchar(124) NOT NULL,
+  `camera_id` int(5) NOT NULL,
+  `traffic_id` int(5) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `camera_id` (`camera_id`),
+  KEY `traffic_id` (`traffic_id`),
+  CONSTRAINT `images_ibfk_1` FOREIGN KEY (`camera_id`) REFERENCES `cameras` (`id`),
+  CONSTRAINT `images_ibfk_2` FOREIGN KEY (`traffic_id`) REFERENCES `traffic` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `images`
+--
+
+LOCK TABLES `images` WRITE;
+/*!40000 ALTER TABLE `images` DISABLE KEYS */;
+/*!40000 ALTER TABLE `images` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `traffic`
+--
+
+DROP TABLE IF EXISTS `traffic`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `traffic` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `detect_date` date NOT NULL,
+  `objet_type` enum('human','animal','car','none') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `traffic`
+--
+
+LOCK TABLES `traffic` WRITE;
+/*!40000 ALTER TABLE `traffic` DISABLE KEYS */;
+/*!40000 ALTER TABLE `traffic` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `email` varchar(64) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `role` enum('admin','regular') NOT NULL,
+  `password` varchar(64) NOT NULL,
+  PRIMARY KEY (`email`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-07-17 21:21:37
