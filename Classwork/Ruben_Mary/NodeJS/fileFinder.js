d35d08c723d020df846576ec7b4278f01e972eeb
Ruben Ghalayan <rubenghalayan@gmail.com>
2017-07-21 15:53:30 +0400

    Added file finder in NodeJS

---
+++ Classwork/Ruben_Mary/NodeJS/fileFinder.js
@@ -0,0 +1,18 @@
+// To execute this script type the following int terminal:
+// node/nodejs finder.js <file name> <file path>
+
+var Finder = require('fs-finder');
+var process = require('process');
+var colors = require('colors/safe');
+
+var myArgs = process.argv.slice(2);
+if (myArgs.length != 2 ) {
+    console.log("Error: Invalid number of arguments");
+} else {
+    console.log(colors.green("Process ID: ", process.pid));
+    Finder.from(myArgs[1]).findFiles(myArgs[0], function(files) {
+        console.log(colors.blue(files));
+        console.log(colors.green("Script runtime: ", process.uptime()));
+        console.log(colors.green("Process memory usage", process.memoryUsage().heapUsed));
+    });
+}
