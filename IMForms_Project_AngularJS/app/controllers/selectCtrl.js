adaaa5bc3b0e55e7fefa1423b0ef1209f9367a14
VrezhUnanyan <vrezhunanyan1997@mail.ru>
2017-07-25 15:05:44 +0400

    Deleted Angular2 project

--- IMForms_Project_AngularJS/app/controllers/selectCtrl.js
+++
@@ -1,9 +0,0 @@
-app.controller("select", ['$scope', function($scope) {
-    $scope.go = function (locate) {
-            window.location = locate;
-    }
-    $scope.hide = function () {
-        $scope.show = false;
-    } 
-    
-}]);
--- IMForms_Project_AngularJS/app/createApp.js
+++
@@ -1,1 +0,0 @@
-var createApp = angular.module("createApp", []);
