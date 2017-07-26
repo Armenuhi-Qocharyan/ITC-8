f0230913a3284ba6a82bfcda3a4fedd3823fd9ed
Ruben <rubenghalayan@mail.ru>
2017-07-26 14:33:18 +0400

    Added nodejs homework with mongoose module

--- Homework/Ruben/nodeJS/mongo/app.js
+++ Homework/Ruben/nodeJS/mongo/app.js
@@ -5,17 +5,16 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
 var users = require('./routes/users');
+
 var http = require ('http');
 var mongoose = require ("mongoose");
 mongoose.Promise = global.Promise;
 
-
 var uristring =
 process.env.MONGOLAB_URI ||
 process.env.MONGOHQ_URL ||
-'mongodb://localhost/nodeApi';
+'mongodb://localhost/myUsers';
 
 mongoose.connect(uristring, function (err, res) {
     if (err) {
@@ -26,10 +25,6 @@ mongoose.connect(uristring, function (err, res) {
 });
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -38,8 +33,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- Homework/Ruben/nodeJS/mongo/routes/users.js
+++ Homework/Ruben/nodeJS/mongo/routes/users.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var myCtrl = require('../api/controllers/myController');
 /* GET users listing. */
-router.get('/', myCtrl.getUsers);
+router.get('/users', myCtrl.getUsers);
+router.post('/users', myCtrl.addUser);
+router.delete('/user/:username', myCtrl.deleteUser);
+router.delete('/user/', myCtrl.deleteAllUsers);
 
 module.exports = router;
