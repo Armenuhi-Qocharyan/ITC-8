8f29eff433cd8902be8c74bc0a006e89fc5acb0a
Khachik Ghazaryan <Khachik.ghazaryan.im@gmail.com>
2017-07-27 11:40:45 +0400

    Corrected node homework for 25/04

--- Homework/Khachik/nodeJs/25.07/nodeApp/controllers/myController.js
+++ Homework/Khachik/nodeJs/25.07/nodeApp/controllers/myController.js
@@ -2,22 +2,16 @@ var model = require('../models/myModel');
 
 
 module.exports.addUser = function(req, res) {
-    //req.body.age = parseInt(req.body age);
-    var r = model.addUser(req);
-    res.send(r);
-//    console.log(req);
+    model.addUser(req,res);
 }
 
 
 module.exports.getUsers = function(req, res) {
-    var r = model.getUsers();
-    res.send(JSON.stringify(r));
+    model.getUsers(req,res);
 } 
 
 module.exports.deleteUser = function(req, res) {
-//    console.log(req.params.username);
-    var r = model.deleteUser(req);
-    res.send(r);
+    model.deleteUser(req,res);
 }
 module.exports.deleteAllUsers = function(req, res) {
 //    model.deleteAllUsers();
--- Homework/Khachik/nodeJs/25.07/nodeApp/models/myModel.js
+++ Homework/Khachik/nodeJs/25.07/nodeApp/models/myModel.js
@@ -15,41 +15,47 @@ var users_table = dao({ db: db, table: 'users_table' });
 
 var b = { name: 'John Public', email : 'user@gmail.com',age: 23 };
 
-module.exports.addUser = function(r) {
-    console.log(r.body);
-    users_table.upsert(r.body).then(function() {
+module.exports.addUser = function(req, res) {
+    console.log(req.body);
+    users_table.upsert(req.body).then(function() {
         console.log("Successfully added");
-        return 'Successfully added.';
+        res.send(JSON.stringify({"status": 200, "body":"Successfully added."}));
+//        res.status(200).json({"added":true, "body":"Successfully added."});
     }) .catch(function () {
-        console.log("Uncorrect request");
+        res.send(JSON.stringify({"status": 400, "body":"bad request"}));
+//        res.status(400).json({"added":false, "body":"Bad request"});
+ 
     });
 };
 
 
-module.exports.getUsers = function() {
+module.exports.getUsers = function(req, res) {
     users_table.find().then(function(us) {
         console.log(us);
-        return us;
+        res.send(JSON.stringify(us));
     }).catch(function () {
-        console.log("Promise Rejected");
+         res.send(JSON.stringify({"status": 404, "body":"Not found"}));
     });
 };
 
 
 
 
-module.exports.deleteUser = function(r) {
+module.exports.deleteUser = function(req, res) {
     db.transaction(function*() {
-        var userName = r.params.username;
+        var userName = req.params.username;
         var b = yield users_table.findOne('name = ?', userName);
-        console.log(b.id);
         yield users_table.delete('id = $id', {"id" : b.id});
     }).then(function(suc) {
-        return '{"status":200, "body":"User successfully deleted"}'
-    }).catch
+          res.send(JSON.stringify({"delete": true, "body":"Successfully deleted"}));
+          console.log("Successfully deleted\n");
+    }).catch(function () {
+         res.send(JSON.stringify({"status": 400, "body":"Bad request"}));
+    });
+ 
 }
 
-module.exports.deleteAllUsers = function() {
+module.exports.deleteAllUsers = function(req, res) {
     db.transaction(function*() {
         yield dao.delete();
     });
