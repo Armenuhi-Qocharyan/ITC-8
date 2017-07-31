9f570d85786c2e43be602bb99beb62d69f5dd472
Albert <albert93aghajanyan@mail.ru>
2017-08-01 01:23:13 +0400

    Added painter homework

---
+++ Homework/Albert/Java/01.08.2017/Shapes/Rectangle.java
@@ -0,0 +1,20 @@
+package Shapes;
+import Shapes.Shape;
+import java.io.PrintStream;
+
+public class Rectangle implements Shape {
+
+    public void draw(PrintStream out, char symbol) {
+        System.out.println("Rectangle: Write to file completed.");
+        int size = 10;
+        char[][] rect = new char[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                out.print(symbol);
+            }
+            out.print("\n");
+        }
+
+
+    }
+}
