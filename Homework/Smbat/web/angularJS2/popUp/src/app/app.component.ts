82395ef3d3227e809d89fbe877a826664131b354
SmbatDallakyan <Smbat.Dallakyan.im@gmail.com>
2017-08-02 12:08:00 +0400

    Angular testing Pop Up

---
+++ Homework/Smbat/web/angularJS2/popUp/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {MdDialogModule} from 'md-dialog/dialog';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
