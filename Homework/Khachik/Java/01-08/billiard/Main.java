dedd211a9283bb90af3167ddb3ee5d7e644dc5c6
Khachik Ghazaryan <Khachik.ghazaryan.im@gmail.com>
2017-08-02 12:55:34 +0400

    Corrected Java program billiard

--- Homework/Khachik/Java/01-08/billiard/Main.java
+++ Homework/Khachik/Java/01-08/billiard/Main.java
@@ -1,13 +1,18 @@
 import balls.Ball;
+import java.util.*;
 
 public class Main{
     public static void main(String[] args) {
         Ball ball1 = new Ball();
         Ball ball2 = new Ball();
-        System.out.println("\nFirst ball:\n");
-        ball1.initialize();
-        System.out.println("\nSecond ball:\n");
-        ball2.initialize();
-        ball1.move(ball2);
+        try {
+            System.out.println("\nFirst ball:\n");
+            ball1.initialize();
+            System.out.println("\nSecond ball:\n");
+            ball2.initialize();
+            ball1.move(ball2);
+        } catch (InputMismatchException ex) {
+            System.out.println("Input Error.");
+        }
     }
 }
--- Homework/Khachik/Java/01-08/billiard/balls/Ball.java
+++ Homework/Khachik/Java/01-08/billiard/balls/Ball.java
@@ -3,15 +3,18 @@ import java.util.*;
 import java.lang.*;
 
 public class Ball{
+    public double radius;
     public double x;
     public double y;
     public double speed;
-    public double angle;
+    public double vectorX;
+    public double vectorY;
     public Ball() {
         this.x = 0;
         this.y = 0;
         this.speed = 0;
-        this.angle = 0;
+        this.vectorX = 0;
+        this.vectorY = 0;
     }
     public void initialize () {
         System.out.print("Input balls coordinates:\nx = ");
@@ -19,10 +22,13 @@ public class Ball{
         this.x = scan.nextInt();
         System.out.print("y = ");
         this.y = scan.nextInt();
-        System.out.print("Input ball speed (cm/sec).\nspeed = ");
-        this.speed = scan.nextInt();
-        System.out.print("Input ball movement angle (0 - 360).\nangle = ");
-        this.angle = scan.nextInt();
+        System.out.print("Input balls radius:\nradius = ");
+        this.radius = scan.nextInt(); 
+        System.out.print("Input vectors coordinates:\nx = ");
+        this.vectorX = scan.nextInt();
+        System.out.print("y = ");
+        this.vectorY = scan.nextInt();
+
     }
     public void move(Ball ball2) {
         double time = 0;
@@ -30,23 +36,33 @@ public class Ball{
         double newY1 = 0;
         double newX2 = 0;
         double newY2 = 0;
+        if (Math.abs(this.x - ball2.x) < (this.radius + ball2.radius) && Math.abs(this.y - ball2.y) < (this.radius + ball2.radius)) {
+            System.out.println("The balls are one on the other.");
+            return;
+        }
+
         System.out.print("\nInput the movement time (sec):\ntime = ");
         Scanner scan = new Scanner(System.in);
         time = scan.nextInt();
-        for(int i = 0; i <= Math.max(this.speed, ball2.speed); ++i ) {
-            if (this.speed >= i) {
-                newX1 = i * Math.cos(Math.toRadians(this.angle)) + this.x;
-                newY1 = i * Math.sin(Math.toRadians(this.angle)) + this.y;
-            }
-            if (ball2.speed >= i) {
-                newX2 = ball2.speed * Math.cos(Math.toRadians(ball2.angle)) + ball2.x;
-                newY2 = ball2.speed * Math.sin(Math.toRadians(ball2.angle)) + ball2.y;
-            }
-            if (newX1 == newX2 && newY1 == newY2) {
-                System.out.print("Balls are collided.\n");
+
+        newX1 = this.x;
+        newY1 = this.y;
+        newX2 = ball2.x;
+        newY2 = ball2.y;
+        for(int i = 0; i < time; ++i ) {
+                newX1 += this.vectorX;
+                newY1 += this.vectorY;
+                newX2 += ball2.vectorX;
+                newY2 += ball2.vectorY;
+            if (Math.abs(newX1 - newX2) < (this.radius + ball2.radius) && Math.abs(newY1 - newY2) < (this.radius + ball2.radius)) {
+                System.out.print("Balls are collided: (" + newX1 + ", " + newY1 + "), " + "(" + newX2 + ", " + newY2 + ")\n");
+                this.vectorX *= -1;
+                this.vectorY *= -1;
+                ball2.vectorX *= -1;
+                ball2.vectorY *= -1;
             }
         }
         System.out.print("First ball's coordinates after movement:\nx = " + newX1 + "\ny = " + newY1 + "\n");
-        System.out.print("First ball's coordinates after movement:\nx = " + newX2 + "\ny = " + newY2 + "\n");
+        System.out.print("Second ball's coordinates after movement:\nx = " + newX2 + "\ny = " + newY2 + "\n");
     }
 }
