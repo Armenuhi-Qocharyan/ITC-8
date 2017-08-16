aff78fd96ce1481593584e6e71cc16e1f24c65da
Mary Antonyan <maryantonyan777@gmail.com>
2017-08-16 17:35:20 +0400

    Added unit tests

--- Classwork/Smbat_Mary/Maven/DependencyTest/MyProject/src/main/java/com/test/App.java
+++ Classwork/Smbat_Mary/Maven/DependencyTest/MyProject/src/main/java/com/test/App.java
@@ -1,14 +1,14 @@
 package com.test;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main( String[] args ) {
         System.out.println( " Hello World from com.test " );
     }
     public static void myFunction() {
 	System.out.println( " my function " );
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
     }
 }
