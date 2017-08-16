eab0e3705ccc34e2580069a5840c054fb609a40d
Liana Andreasyan <liana.andreasyan.im@gmail.com>
2017-08-16 19:08:11 +0400

    added json

--- Homework/Liana/java/maven/15-08/json/src/main/java/com/myJson/App.java
+++ Homework/Liana/java/maven/15-08/json/src/main/java/com/myJson/App.java
@@ -9,26 +9,40 @@ import java.io.FileWriter;
 import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.Scanner;
+
 
 public class App {
 
-    public static void main(String[] args) {
-        reader();
+    public static void main(String[] args)  {
+        System.out.print("Enter file path: ");
+        Scanner sc = new Scanner(System.in);
+        String path = sc.next();
+        System.out.print("Enter name to get object: ");
+        String name = sc.next();
+        JSONObject myobj = reader(path, name);
+        if(myobj != null) {
+            Object res = print(myobj, "homeworks");
+            System.out.println(res);
+        }
+      //  Object res = print(myobj, "name");
     }
 
-    public static void reader () {
+    public static JSONObject reader (String path, String anun)  {
          JSONParser parser = new JSONParser();
 
         try {
 
-           Object obj = parser.parse(new FileReader("./data.json"));
+           Object obj = parser.parse(new FileReader(path));
            JSONArray jArray = (JSONArray) obj;
                     
            Iterator<JSONObject> iterator = jArray.iterator();
             while (iterator.hasNext()) {
                 JSONObject a = iterator.next();
                 String name = (String) a.get("name");
-                if (name.equals("aa")) {
+                if (name.equals(anun)) {
+                    return a;
+                    /*
                     System.out.print("name: " + name + ",\t");
                     String surname = (String) a.get("surname");
                     System.out.print("surname: " + surname + ",\t");
@@ -40,17 +54,21 @@ public class App {
                     }
 
                     System.out.println();
+                     */
                 }
             }
             
-           writer(jArray);
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
+            return null;
         } catch (IOException e) {
             System.out.println("Can't read file");
+            return null;
         } catch (ParseException e) {
             System.out.println("Can't parse json");
+            return null;
         }
+        return null;
 
     }
 
@@ -75,5 +93,10 @@ public class App {
         } catch (IOException e) {
             System.out.println("Can't write in file");
         }
+    }
+    
+    public static Object print(JSONObject obj, String type) {
+           return obj.get(type);
+       
     }
 }
