8b41b13d90e80f7cf3d7b774707aa1f10fa87593
Albert <albert93aghajanyan@mail.ru>
2017-08-17 17:52:07 +0400

    maven

--- Classwork/Abo_Vrezh/maven/MyJsonReaderWriter/src/main/java/com/app/App.java
+++ Classwork/Abo_Vrezh/maven/MyJsonReaderWriter/src/main/java/com/app/App.java
@@ -59,5 +59,9 @@ public class App
     public static int sum(int a, int b) {
         return a+b;
     }
+    public static int sub(int a, int b) {
+        return a-b;
+    }
+
 
 }
--- Classwork/Abo_Vrezh/maven/MyJsonReaderWriter/src/test/java/com/app/AppTest.java
+++ Classwork/Abo_Vrezh/maven/MyJsonReaderWriter/src/test/java/com/app/AppTest.java
@@ -1,67 +1,36 @@
 package com.app;
 
-import org.junit.Test;
+//import org.junit.Test;
 //import org.junit.TestCase;
 //import org.junit.TestSuite;
-import static org.junit.Assert.*;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.json.simple.JSONObject;
+//import org.junit.After;
+//import org.junit.AfterClass;
+//import org.junit.Before;
+//import org.junit.BeforeClass;
+//import org.json.simple.JSONObject;
 
-/**
- * Unit test for simple App.
- */
+import static org.junit.Assert.*;
+import org.testng.annotations.*;
 
 //extends TestCase 
 public class AppTest {
-    App app;
 
-    @Before
-    public void init() {
-        app = new App();
-        System.out.println("Before");
-    }
-
-    @Test
-    public void testGetObject() {
-        JSONObject jsonObj = new JSONObject("[{\"homeworks\":[\"h1\",\"h2\",\"h3\"],\"surname\":\"sn1\",\"name\":\"n1\"}]");
-            
-        assertEquals(jsonObj, app.getObject(app.getJSONArray, "name"));
-    } 
-
-    @Test
-    public void testNameOrSurname() {
-        assertEquals("sn1", app.getNameOrSurname(app.getJSONObject(), "name"));
-    }
-
-
-
-    /*
-    @Test
+    @Test(groups="group1")
     public void testSum1() {
-        System.out.println("Test");
+        System.out.println("test 1   sum");
         assertEquals(50, com.app.App.sum(20,30));
     }
-
-    @After
-    public void testSum2() {
-        System.out.println("After");
-    }
-
-    @Before
+    @Test(groups="group2")
     public void testSum3() {
-        System.out.println("Before");
-    }
-    @BeforeClass
-    public static void testSum4() {
-        System.out.println("Before Class");
+        System.out.println("test 3   sum");
+        assertEquals(5, com.app.App.sum(2,3));
     }
 
-    @AfterClass
-    public static void testSum5() {
-        System.out.println("After Class");
+    @Parameters({ "first-name1" })
+    @Test
+    public void testString(String firstName) {
+        System.out.println("Invoked testString " + firstName);
+        assert "Cedric".equals(firstName);
     }
-    */
+
 }
