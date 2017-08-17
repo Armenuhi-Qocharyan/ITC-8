04e38d24ef4c08b8e8753467a188914d3c3dada1
Albert <albert93aghajanyan@mail.ru>
2017-08-17 22:03:15 +0400

    Added maven tests

--- Homework/Albert/Java/17.08.2017/MyJsonReaderWriter/src/test/java/com/app/AppTest.java
+++ Homework/Albert/Java/17.08.2017/MyJsonReaderWriter/src/test/java/com/app/AppTest.java
@@ -7,15 +7,50 @@ package com.app;
 //import org.junit.AfterClass;
 //import org.junit.Before;
 //import org.junit.BeforeClass;
-//import org.json.simple.JSONObject;
-//import org.json.simple.JSONArray;
-//import java.io.IOException;
-//import org.json.simple.parser.ParseException;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import java.io.IOException;
+import org.json.simple.parser.ParseException;
 import static org.junit.Assert.*;
 import org.testng.annotations.*;
 
 
 public class AppTest {
+
+    App app;
+    JSONObject obj = new JSONObject();
+
+    @BeforeClass
+    public void init() {
+        //System.out.println("Before");
+        app = new App();
+        obj = new JSONObject();
+        obj.put("name","n1");
+        obj.put("surname","sn1");
+        JSONArray homeworks = new JSONArray();
+        homeworks.add("h1");
+        homeworks.add("h2");
+        homeworks.add("h3");
+        obj.put("homeworks", homeworks);
+    }
+
+    @Test
+    public void testGetObject() {
+        //JSONObject jsonObj = new JSONObject("[{\"homeworks\":[\"h1\",\"h2\",\"h3\"],\"surname\":\"sn1\",\"name\":\"n1\"}]");
+        try {
+            assertEquals(obj, app.getObject(app.getJSONArray(), "n1"));
+        } catch (IOException e) {
+            System.out.println(e);
+        } catch (ParseException e) {
+            System.out.println(e);
+        }
+    } 
+
+    @Test
+    public void testGetNameOrSurname() {
+        assertEquals("n1", app.getNameOrSurname(obj,"name"));
+    }
+
 
     @Test(groups="group1")
     public void testSum1() {
@@ -55,12 +90,12 @@ public class AppTest {
     public void afterGroups() {
         System.out.println("@AfterGroups");
     }
-
+/*
     @BeforeClass
     public void beforeClass() {
         System.out.println("@BeforeClass");
     }
-
+*/
     @BeforeTest
     public void beforeTest() {
         System.out.println("@BeforeTest");
@@ -87,39 +122,3 @@ public class AppTest {
     }
 }
 
-/*
-    App app;
-    JSONObject obj = new JSONObject();
-
-    @Before
-    public void init() {
-        //System.out.println("Before");
-        app = new App();
-        obj = new JSONObject();
-        obj.put("name","n1");
-        obj.put("surname","sn1");
-        JSONArray homeworks = new JSONArray();
-        homeworks.add("h1");
-        homeworks.add("h2");
-        homeworks.add("h3");
-        obj.put("homeworks", homeworks);
-    }
-
-    @Test
-    public void testGetObject() {
-        //JSONObject jsonObj = new JSONObject("[{\"homeworks\":[\"h1\",\"h2\",\"h3\"],\"surname\":\"sn1\",\"name\":\"n1\"}]");
-        try {
-            assertEquals(obj, app.getObject(app.getJSONArray(), "n1"));
-        } catch (IOException e) {
-            System.out.println(e);
-        } catch (ParseException e) {
-            System.out.println(e);
-        }
-    } 
-
-    @Test
-    public void testGetNameOrSurname() {
-        assertEquals("n1", app.getNameOrSurname(obj,"name"));
-    }
-
-*/
