77053519d99ed0a5c54c058ab7a4b9438b269e9d
Vrezh Unanyan <vrezhunanyan1997@mail.ru>
2017-08-22 02:24:36 +0400

    Created 'Image Editor' and 'Video images save' programs.

---
+++ Homework/Vrezh/openCV/saveVideoImages/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <opencv2/opencv.hpp>
+
+int main (int argc, char **argv) {
+    cv::VideoCapture cap(0);
+    if (!cap.isOpened()) {
+        std::cerr << "ERROR: Could not open video " << argv[1] << std::endl;
+        return 1;
+    }
+
+    int i = 0;
+    while(i <= 3) {
+        cv::Mat frame;
+        cap >> frame;
+        std::string filename = "frame_" + std::to_string(i) + ".jpg";
+        cv::imwrite(filename, frame);
+        ++i;
+    }
+
+  return 0;
+}
