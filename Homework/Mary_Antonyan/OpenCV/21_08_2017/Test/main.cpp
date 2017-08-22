7c319ba19267d9096a606df8f3808d3ca193fd7f
Mary Antonyan <maryantonyan777@gmail.com>
2017-08-22 14:30:23 +0400

    Finalized Test project by adding drawing and saving functionality

--- Homework/Mary_Antonyan/OpenCV/21_08_2017/Test/main.cpp
+++ Homework/Mary_Antonyan/OpenCV/21_08_2017/Test/main.cpp
@@ -4,7 +4,7 @@
 using namespace cv;
 
 int main(int argc, char** argv) {
-    if ( argc != 2 ) {
+    if (argc != 2) {
         std::cout << "Error: Missing command line argumanets.\n\tUsage: DisplayImage.out <Image_Path>" << std::endl;
         return -1;
     }
@@ -17,9 +17,28 @@ int main(int argc, char** argv) {
         std::cout << "Found image ^^" << std::endl;
     }
 
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
-    waitKey(0);
+    namedWindow("Test image", WINDOW_AUTOSIZE);
+    // Draw line
+    line(image, *(new Point(0,0)), *(new Point(511,511)), *(new Scalar(255,0,0)), 5);
 
+    //Draw rectangle
+    rectangle(image,*(new Point(50,0)), *(new Point(150,100)), *(new Scalar(0,255,0)), 3);
+
+    // Draw circle
+    circle(image,*(new Point(447,63)), 63, *(new Scalar(0,0,255)), -1);
+
+    // Show image with drawings
+    imshow("Test image", image);
+
+    // Modify image to grayscale
+    namedWindow("Grayscale image", WINDOW_AUTOSIZE);
+    Mat grayImage;
+    cvtColor(image, grayImage, CV_BGR2GRAY);
+    imshow("Grayscale image", grayImage);
+
+    // Save grayscale image
+    imwrite("./gray_image.jpg", grayImage);
+
+    waitKey(0);
     return 0;
 }
