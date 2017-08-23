a9093bd8db2dc3663cebd059382a6b31e2d9fa78
Ruben Ghalayan <rubenghalayan@mail.ru>
2017-08-23 17:53:10 +0400

    Added opencv detected image

---
+++ Homework/Ruben/openCV/dog/main.cpp
@@ -0,0 +1,93 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace cv;
+using namespace std;
+
+Mat src; Mat src_gray;
+int thresh = 200;
+int max_thresh = 255;
+RNG rng(12345);
+
+/// Function header
+void thresh_callback(int, void* );
+
+int main( int argc, char** argv )
+{
+  /// Load source image and convert it to gray
+  src = imread( argv[1], 1 );
+
+  /// Convert image to gray and blur it
+  cvtColor( src, src_gray, CV_BGR2GRAY );
+//  blur( src_gray, src_gray, Size(20,30) );
+
+  /// Create Window
+  char* source_window = "Source";
+  namedWindow( source_window, CV_WINDOW_AUTOSIZE );
+  imshow( source_window, src );
+
+  createTrackbar( " Canny thresh:", "Source", &thresh, max_thresh, thresh_callback );
+  thresh_callback( 0, 0 );
+
+  waitKey(0);
+  return(0);
+}
+
+void thresh_callback(int, void* )
+{
+  Mat canny_output;
+  vector<vector<Point> > contours;
+  vector<Vec4i> hierarchy;
+
+  /// Detect edges using canny
+  Canny( src_gray, canny_output, thresh, thresh*3, 5 );
+  /// Find contours
+  findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(10, 10) );
+
+  /// Draw contours
+  Mat drawing = Mat::zeros( canny_output.size(), CV_8UC1 );
+  for( int i = 0; i< contours.size(); i++ )
+     {
+       Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+       drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
+     }
+
+  /// Show in a window
+  namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
+  imshow( "Contours", drawing );
+}
+/*
+Mat gray(Mat image){
+    Mat gray_image;
+    cvtColor( image, gray_image, CV_BGR2GRAY );
+//    namedWindow( "Gray image", WINDOW_NORMAL );
+    return gray_image;
+}
+
+Mat blur(Mat image, int size){
+   Mat dst;
+   blur( image, dst, Size( size, size ) );
+   return dst;
+}
+
+void print(String name, Mat image){
+    imshow( name, image );
+}
+
+int main( int argc, char** argv ) {
+    char* imageName = argv[1];
+    Mat image;
+    image = imread( imageName, 1 );
+    if ( argc != 2 || !image.data ) {
+        printf( " No image data \n " );
+        return -1;
+    }
+  //  Mat newImage = gray(image);
+  //  print("image", blur(newImage,10));
+ 
+    waitKey(0);
+    return 0;
+}*/
