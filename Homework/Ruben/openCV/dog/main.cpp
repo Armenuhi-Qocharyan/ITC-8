c971db4c76d62b29873686505906851f8ef4b094
Ruben <rubenghalayan@mail.ru>
2017-08-24 04:24:36 +0400

    Added opencv homework

--- Homework/Ruben/openCV/dog/main.cpp
+++ Homework/Ruben/openCV/dog/main.cpp
@@ -1,5 +1,5 @@
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,87 +7,58 @@
 using namespace cv;
 using namespace std;
 
-Mat src; Mat src_gray;
-int thresh = 200;
-int max_thresh = 255;
-RNG rng(12345);
+Mat src; 
+Mat src_gray;
 
-/// Function header
 void thresh_callback(int, void* );
 
-int main( int argc, char** argv )
-{
-  /// Load source image and convert it to gray
-  src = imread( argv[1], 1 );
 
-  /// Convert image to gray and blur it
-  cvtColor( src, src_gray, CV_BGR2GRAY );
-//  blur( src_gray, src_gray, Size(20,30) );
-
-  /// Create Window
-  char* source_window = "Source";
-  namedWindow( source_window, CV_WINDOW_AUTOSIZE );
-  imshow( source_window, src );
-
-  createTrackbar( " Canny thresh:", "Source", &thresh, max_thresh, thresh_callback );
-  thresh_callback( 0, 0 );
-
-  waitKey(0);
-  return(0);
+int main( int argc, char** argv ) {
+    src = imread( argv[1], 1 );
+    if (!src.data) {
+        std::cout << "Image file not found\n";
+        return 1;
+    }
+    cvtColor( src, src_gray, CV_BGR2GRAY );
+    for(int i = 0 ; i < 20; i++){    
+       blur( src_gray, src_gray, Size(4,4) );
+    }
+    thresh_callback( 0, 0 );
+    waitKey(0);
+    return(0);
 }
 
-void thresh_callback(int, void* )
-{
+void thresh_callback(int, void* ) {
   Mat canny_output;
   vector<vector<Point> > contours;
   vector<Vec4i> hierarchy;
 
-  /// Detect edges using canny
-  Canny( src_gray, canny_output, thresh, thresh*3, 5 );
-  /// Find contours
-  findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(10, 10) );
-
-  /// Draw contours
-  Mat drawing = Mat::zeros( canny_output.size(), CV_8UC1 );
-  for( int i = 0; i< contours.size(); i++ )
-     {
-       Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-       drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
-     }
-
-  /// Show in a window
-  namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
-  imshow( "Contours", drawing );
-}
-/*
-Mat gray(Mat image){
-    Mat gray_image;
-    cvtColor( image, gray_image, CV_BGR2GRAY );
-//    namedWindow( "Gray image", WINDOW_NORMAL );
-    return gray_image;
-}
-
-Mat blur(Mat image, int size){
-   Mat dst;
-   blur( image, dst, Size( size, size ) );
-   return dst;
-}
-
-void print(String name, Mat image){
-    imshow( name, image );
-}
-
-int main( int argc, char** argv ) {
-    char* imageName = argv[1];
-    Mat image;
-    image = imread( imageName, 1 );
-    if ( argc != 2 || !image.data ) {
-        printf( " No image data \n " );
-        return -1;
+  Canny( src_gray, canny_output, 30, 30, 3 );
+  findContours( canny_output, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE, Point(0, 0) );
+    Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+    for( int i = 0; i< contours.size(); i++ ) {
+       Scalar color = Scalar( 255,255,0 );
+       drawContours( drawing, contours, i, color, 10, 8, hierarchy, 0, Point() );
     }
-  //  Mat newImage = gray(image);
-  //  print("image", blur(newImage,10));
  
-    waitKey(0);
-    return 0;
-}*/
+    cvtColor( drawing, drawing, CV_BGR2GRAY );
+    Canny( drawing, drawing, 10, 10, 3 );
+    findContours( drawing, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE, Point(0, 0) );
+    drawing = Mat::zeros( drawing.size(), CV_8UC3 );
+    std::vector<cv::Point> man;
+    cv::Rect rectangle;
+    for( int i = 0; i< contours.size(); i++ ) {
+        Scalar color = Scalar( 255,i%2*200,0 );
+        rectangle = cv::boundingRect(contours[i]);
+        if (rectangle.width > 40 &&  rectangle.height > 300) {
+            man.push_back(rectangle.tl());
+            man.push_back(rectangle.br());
+            drawContours( drawing, contours, i, color, 10, 8, hierarchy, 0, Point() );
+            cv::Rect rectangleMan = cv::boundingRect(man);
+            cv::rectangle(src, rectangleMan.tl(), rectangleMan.br(), cv::Scalar(128,0,128),3);
+        }
+    }
+
+    namedWindow( "image", CV_WINDOW_NORMAL );
+    imshow( "image",  src);
+}
