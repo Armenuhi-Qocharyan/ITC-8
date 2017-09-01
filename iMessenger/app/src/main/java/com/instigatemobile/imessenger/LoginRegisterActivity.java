429356c8a44f5e0da9345a363da06f9bb949441b
Albert <albert93aghajanyan@mail.ru>
2017-09-02 01:11:19 +0400

    Added LoginRegisterActivity

--- iMessenger/app/src/main/java/com/instigatemobile/imessenger/LoginRegisterActivity.java
+++ iMessenger/app/src/main/java/com/instigatemobile/imessenger/LoginRegisterActivity.java
@@ -5,9 +5,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.basgeekball.awesomevalidation.AwesomeValidation;
 import com.instigatemobile.imessenger.profile.LoginFragment;
-import com.instigatemobile.imessenger.profile.RegisterFragment;
 
 public class LoginRegisterActivity extends AppCompatActivity {
     //private RegisterFragment registerFragment;
@@ -18,7 +16,7 @@ public class LoginRegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login_register);
         //registerFragment = new RegisterFragment();
         loginFragment = new LoginFragment();
         FragmentManager fragmentManager =getSupportFragmentManager();
