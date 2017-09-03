43752b83fa6c73dc062d6ba31d007d8feb447d02
Vachagan Hovhannisyan <vachagan.hovhannisyan.im@gmail.com>
2017-09-03 22:44:36 +0400

    added contacts page example

--- iMessenger/app/src/main/java/com/instigatemobile/imessenger/Contacts.java
+++ iMessenger/app/src/main/java/com/instigatemobile/imessenger/Contacts.java
@@ -6,9 +6,13 @@ package com.instigatemobile.imessenger;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.instigatemobile.imessenger.Adapters.PersonAdapter;
 
 public class Contacts extends Fragment {
 
@@ -16,6 +20,9 @@ public class Contacts extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_contacts, container, false);
+        RecyclerView recyclerView =(RecyclerView) rootView.findViewById(R.id.my_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(new PersonAdapter());
         return rootView;
     }
 }
