8d61b5067452b73b00af6e7ae9dce5b421e35ad2
Mary Antonyan <maryantonyan777@gmail.com>
2017-09-18 17:00:14 +0400

    Updated logic in Main clas

--- Classwork/Smbat_Mary/Design_Patterns/FactoryDesignPattern/Main.java
+++ Classwork/Smbat_Mary/Design_Patterns/FactoryDesignPattern/Main.java
@@ -1,23 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        UniversityFactory university = UniversityFactory.getUniversity("Polytechnic");
+        showProfessions("Polytechnic");
+        showProfessions("Pedagogical");
+    }
+
+    public static void showProfessions(String univer) {
+        UniversityFactory university = UniversityFactory.getUniversity(univer);
         if (university == null) {
             return;
         }
-        Programmer polytechnicProgrammer = university.getProgrammer();
-        Physicist polytechnicPhysicist = university.getPhysicist();
+        Programmer programmer = university.getProgrammer();
+        Physicist physicist = university.getPhysicist();
 
-        polytechnicProgrammer.draw();
-        polytechnicPhysicist.draw();
-
-        university = UniversityFactory.getUniversity("Pedagogical");
-        if (university == null) {
-            return;
-        }
-        Programmer pedagogicProgrammer = university.getProgrammer();
-        Physicist pedagogicPhysicist = university.getPhysicist();
-
-        pedagogicProgrammer.draw();
-        pedagogicPhysicist.draw();
+        programmer.draw();
+        physicist.draw();
     }
 }
