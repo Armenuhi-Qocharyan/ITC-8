5048db089a10371a4b8d2e69bf79ac80813f659e
Albert Aghajanyan <albert93aghajanyan@mail.ru>
2017-10-06 05:38:34 +0400

    Added delete obj and edit obj cucumber test

--- Homework/Albert/Cucumber/ObjectPageTest/src/test/java/ObjectPageScenario.java
+++ Homework/Albert/Cucumber/ObjectPageTest/src/test/java/ObjectPageScenario.java
@@ -1,7 +1,8 @@
 import cucumber.annotation.en.Given;
 import cucumber.annotation.en.Then;
 import cucumber.annotation.en.When;
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,40 +11,73 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class ObjectPageScenario {
     
     private final WebDriver driver;
-
+    private int countObj;
     public ObjectPageScenario() {
         this.driver = new ChromeDriver();
+        this.countObj = 0;
     }
     
-    @Given("^the page is open \"([^\"]*)\"$")
-    public void the_page_is_open(String page) throws Throwable {
+    @Given("^I navigate to \"([^\"]*)\"$")
+    public void i_navigate_to(String page) throws Throwable {
     	driver.get(page);
     }
-
-    @When("^I enter email \"([^\"]*)\"$")
-    public void I_enter_email(String email) throws Throwable {
-        WebElement element = driver.findElement(By.xpath("//*[@id=\"inputEmail\"]"));
-	element.sendKeys(email);
-    }
-        
-    @When("^I enter password \"([^\"]*)\"$")
-    public void I_enter_password(String password) throws Throwable {
-        WebElement element = driver.findElement(By.xpath("//*[@id=\"inputPassword\"]"));
-	element.sendKeys(password);
-    }
-        
-    @When("^I enter signin button")
-    public void I_enter_signin_butoon() throws Throwable {
-        WebElement element = driver.findElement(By.xpath("//*[@id=\"loginForm\"]/form/button"));
-	element.click();
-    }
-        
-    @Then("^i navigate home page \"([^\"]*)\"$")
-    public void i_navigate_home_page(String pageUrl) throws Throwable {
-        System.out.println("url: " + pageUrl);
-        System.out.println("drive : " + driver.getCurrentUrl());
-        Thread.sleep(2000);
-        assertTrue(driver.getCurrentUrl().contains(pageUrl));
+    
+    @When("^I click button by xpath \"([^\"]*)\"$")
+    public void I_click_button_by_xpath(String buttonXpath) throws Throwable {
+        WebElement clickedButton = driver.findElement(By.xpath(buttonXpath));
+        clickedButton.click();
+        Thread.sleep(300);
     }
     
+    @Then("^I should navigate to \"([^\"]*)\" page$")
+    public void i_should_navigate_to_page(String pageUrl) throws Throwable {
+        Assert.assertEquals("http://localhost:4200/" + pageUrl, driver.getCurrentUrl());
+        Thread.sleep(500);
+        if (driver.getCurrentUrl().equals("http://localhost:4200/detected")){
+            ArrayList<WebElement> countObjList = (ArrayList<WebElement>) driver.findElements(By.xpath("//*[@id=\"obj-area\"]/div/div"));
+            this.countObj = countObjList.size();
+        }
+    }
+    @When("^I enter the email and password \"([^\"]*)\" \"([^\"]*)\"$")
+    public void I_enter_the_email_and_password(String inputEmail, String inputPass) {
+        WebElement emailPass = driver.findElement(By.xpath(inputEmail));
+        emailPass.sendKeys("admin@mail.ru");
+        emailPass = driver.findElement(By.xpath(inputPass));
+        emailPass.sendKeys("Admin1");
+    }
+    
+    @Then("^the count of objects will decrease by one")
+    public void the_count_of_objects_will_decrease_by_one() throws Throwable {
+        ArrayList<WebElement> countObjList = (ArrayList<WebElement>) driver.findElements(By.xpath("//*[@id=\"obj-area\"]/div/div"));
+        Assert.assertEquals(this.countObj-1, countObjList.size());
+    }
+    
+    @Then("^I enter new type xpath \"([^\"]*)\" value \"([^\"]*)\"$")
+    public void I_enter_new_type_xpath_value(String xpath, String values) {
+        WebElement inputType = driver.findElement(By.xpath(xpath));
+        inputType.clear();
+        inputType.sendKeys(values);
+    }
+    
+    @Then("^I must see \"([^\"]*)\" by xpath \"([^\"]*)\"$")
+    public void I_must_see_this_result(String value, String xpath) {
+        WebElement objType = driver.findElement(By.xpath(xpath));
+        Assert.assertEquals(objType.getText(),value);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Homework/Albert/Cucumber/ObjectPageTest/src/test/resources/com/itc/objectpagetest/cucumber/object.feature
+++ Homework/Albert/Cucumber/ObjectPageTest/src/test/resources/com/itc/objectpagetest/cucumber/object.feature
@@ -5,8 +5,21 @@ Feature: Test detected objects page
   Test merge object
 
   Scenario: Test login
-    Given the page is open "http://localhost:4200/login"
-    And I enter email "admin@mail.ru"
-    And I enter password "Admin1"
-    And I enter signin button
-    Then i navigate home page "http://localhost:4200/home"
+    Given I navigate to "http://localhost:4200/home"
+    When I click button by xpath "/html/body/id-root/div/div/id-header/header/nav/div/div[2]/ul[2]/a"
+    Then I should navigate to "login" page
+    When I enter the email and password "//*[@id='inputEmail']" "//*[@id='inputPassword']"
+    And  I click button by xpath "/html/body/id-root/div/div/id-login/div/div/form/button"
+    Then I should navigate to "home" page
+    When I click button by xpath "/html/body/id-root/div/div/id-header/header/nav/div/div[2]/ul[1]/li[2]/a"
+    Then I should navigate to "detected" page
+
+    When I click button by xpath "//*[@id='obj-area']/div/div/div[2]/button[1]"
+    Then the count of objects will decrease by one
+
+    When I click button by xpath "//*[@id='obj-area']/div/div/div[2]/button[2]"
+    Then I enter new type xpath "//*[@id='edit']/div/div/form/div[1]/div/div[1]/input" value "car"
+    And I click button by xpath "//*[@id='edit']/div/div/form/div[2]/button[1]"
+    Then I must see "car" by xpath "//*[@id='obj-area']/div/div/div[1]/h6[1]"
+ 
+
